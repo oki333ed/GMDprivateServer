e399d1cc91c74317b43ca474cc634f87dcd7d6cb
oki3ed... <kokosik3215@gmail.com>
2026-01-29 09:04:15 +0200

    2.208 New socials media

--- incl/profiles/updateGJAccSettings.php
+++ incl/profiles/updateGJAccSettings.php
@@ -12,8 +12,12 @@ $cS = ExploitPatch::remove($_POST["cS"]);
 $youtubeurl = ExploitPatch::remove($_POST["yt"]);
 $twitter = ExploitPatch::remove($_POST["twitter"]);
 $twitch = ExploitPatch::remove($_POST["twitch"]);
+$instagram = ExploitPatch::remove($_POST["instagram"]);
+$tiktok = ExploitPatch::remove($_POST["tiktok"]);
+$discordusername = ExploitPatch::remove($_POST["discord"]);
+$custom = ExploitPatch::remove($_POST["custom"]);
 
-$query = $db->prepare("UPDATE accounts SET mS=:mS, frS=:frS, cS=:cS, youtubeurl=:youtubeurl, twitter=:twitter, twitch=:twitch WHERE accountID=:accountID");
-$query->execute([':mS' => $mS, ':frS' => $frS, ':cS' => $cS, ':youtubeurl' => $youtubeurl, ':accountID' => $accountID, ':twitch' => $twitch, ':twitter' => $twitter]);
+$query = $db->prepare("UPDATE accounts SET mS=:mS, frS=:frS, cS=:cS, youtubeurl=:youtubeurl, twitter=:twitter, twitch=:twitch, instagram=:instagram, tiktok=:tiktok, discordusername=:discordusername, custom=:custom WHERE accountID=:accountID");
+$query->execute([':mS' => $mS, ':frS' => $frS, ':cS' => $cS, ':youtubeurl' => $youtubeurl, ':twitter' => $twitter, ':twitch' => $twitch, ':instagram' => $instagram, ':tiktok' => $tiktok, ':discordusername' => $discordusername, ':custom' => $custom, ':accountID' => $accountID]);
 echo 1;
 ?>
